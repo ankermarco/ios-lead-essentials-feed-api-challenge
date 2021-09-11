1995c6397d53287564fe1dc53855e809e9f05a09
Ke Ma <kema2012@gmail.com>
2021-09-11 08:20:59 +0100

    Use internal type Item for Decodable so that don't force FeedImage to conform to Decodable, don't couple the backend data with FeedImage (the app data model)

--- FeedAPIChallenge/Feed API/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -10,14 +10,32 @@ import Foundation
 
 internal struct FeedImageMapper {
 	private struct Root: Decodable {
-		let items: [FeedImage]
+		let items: [Item]
+
+		var imageItems: [FeedImage] {
+			items.map { $0.feedImage }
+		}
 	}
-	
+
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var feedImage: FeedImage {
+			FeedImage(id: image_id,
+			          description: image_desc,
+			          location: image_loc,
+			          url: image_url)
+		}
+	}
+
 	static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
 		guard response.statusCode == 200,
-			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
+		      let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
-		return .success(root.items)
+		return .success(root.imageItems)
 	}
 }
