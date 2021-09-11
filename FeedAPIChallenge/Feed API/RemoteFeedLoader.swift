4f5e9b19434dbaf420f0bfa9a2cedf5d78303fbe
Ke Ma <kema2012@gmail.com>
2021-09-11 08:27:50 +0100

    Do not deliver result after SUT instance has been deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			if self == nil {
+				return
+			}
 
 			switch result {
 			case .failure:
